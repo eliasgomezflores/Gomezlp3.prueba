fa106d34a1fe48069d67755bebeb741529b62a8a
Gomez Elias <elias.f.g.61@gmail.com>
2020-07-09 15:03:51 -0500

    Metodo para determinar aprobacion listo

--- aprobado.py
+++ aprobado.py
@@ -3,13 +3,14 @@
 
 
 
-def determinaraprobaddo(promedio):
+def determinaraprobado(promedio):
     if promedio>=11:
         resultado="Aprobado"
     else:
         resultado="Desaprobado"
     return resultado
 
+print(determinaraprobado(15))
 
 
 
